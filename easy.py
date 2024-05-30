99d26208978ed63e1546e21b0dc7b14e9ad19820
Deivid Silva <146739515+deivsilv@users.noreply.github.com>
2024-05-30 16:53:42 -0300

    Add files via upload

---
+++ easy.py
@@ -0,0 +1,30 @@
+print("olá qual o se nome?")
+
+pergunta = input("diga seu nome:")
+
+print(f"a, que prazer te conhecer {pergunta}")
+
+idade = "quantos anos você tem?"
+
+print(idade)
+
+idade_usuario = input("diga sua idade:")
+
+print(f"a, não parece ter {idade_usuario}")
+
+duvida = "poderia me ajudar com uma duvida? queria saber o preço do litro de leite nos dias atuais?"
+
+print(duvida)
+
+duvida_2 = "poderia me informar o preço do leite nos dias atuais?" 
+
+ajuda_com_duvida = input("informe o preço do leite:")
+
+print(duvida_2)
+
+print(ajuda_com_duvida)
+
+print(f"nossa muito obrigado por me falar que o leite está {ajuda_com_duvida}, agrdeço muito!")
+
+print(f"tenha um bom dia! {pergunta}")
+
